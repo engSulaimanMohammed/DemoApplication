331e8c9d4496600269c5a6b868732b6edf9e4805
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-25 16:37:17 +0400

    feat(task): Add GetMapping to To get one Task by ID

--- demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -1,10 +1,14 @@
 package com.cl.demo.controllers;
 
+import com.cl.demo.entities.Task;
 import com.cl.demo.requestobjects.TaskCreateRequest;
+import com.cl.demo.responseobjects.TaskCreateResponse;
 import com.cl.demo.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -19,6 +23,20 @@ public class TaskController {
     public Map<String, String> addTask(@RequestBody TaskCreateRequest requestObj) {
         return taskService.addTask(requestObj);
     }
+
+    // To get one Task by ID.
+    @GetMapping("/getById")
+    public TaskCreateResponse getTaskById(@RequestParam String uuid) {
+        Task task = taskService.getTaskById(uuid);
+        return TaskCreateResponse.convert(task);
+    }
+
+
+
+
+
+
+
 
 
 
