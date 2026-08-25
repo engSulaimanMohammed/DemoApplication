a85e28523b7e50b8506288196c5e10a1d36be0f0
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-25 16:00:12 +0400

    feat(task): Add Autowired in TaskControlle

--- demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -1,4 +1,13 @@
 package com.cl.demo.controllers;
 
+import com.cl.demo.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+
+
 }
