1db4d0c848fe18b56f436f0e7b0f66c080f8358f
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-25 12:02:24 +0400

    feat(task):Done  TaskCreateRequest

--- demo/src/main/java/com/cl/demo/requestobjects/TaskCreateRequest.java
+++ demo/src/main/java/com/cl/demo/requestobjects/TaskCreateRequest.java
@@ -1,4 +1,28 @@
 package com.cl.demo.requestobjects;
 
+
+import com.cl.demo.entities.TaskStatus;
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class TaskCreateRequest {
+
+
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Date startDate;
+    private TaskStatus taskStatus;
+    private Boolean isAssigned;
+
+
+
+
+
+
 }
