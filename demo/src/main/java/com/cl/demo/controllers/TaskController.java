183d2a5bcfe292f7bd7bd13bbcc32c4c3aefbeb9
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-25 16:03:33 +0400

    feat(task): Add PostMapping to add new task

--- demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -1,12 +1,27 @@
 package com.cl.demo.controllers;
 
+import com.cl.demo.requestobjects.TaskCreateRequest;
 import com.cl.demo.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 public class TaskController {
 
+   // To connect TaskController with TaskService.
     @Autowired
     private TaskService taskService;
+
+    // To add a new Task.
+    @PostMapping("/add")
+    public Map<String, String> addTask(@RequestBody TaskCreateRequest requestObj) {
+        return taskService.addTask(requestObj);
+    }
+
+
+
 
 
 
