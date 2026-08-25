4f525d2720d10bceddc0c5b5d7bcfb3db63214e5
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-25 14:37:44 +0400

    feat(task): add task request objects

--- demo/src/main/java/com/cl/demo/requestobjects/TaskUpdateRequest.java
+++ demo/src/main/java/com/cl/demo/requestobjects/TaskUpdateRequest.java
@@ -1,4 +1,31 @@
 package com.cl.demo.requestobjects;
 
+
+import com.cl.demo.entities.TaskStatus;
+import java.util.Date;
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
 public class TaskUpdateRequest {
+
+    private UUID uuid;
+    private String titleToUpdate;
+    private String descriptionToUpdate;
+    private TaskStatus taskStatusToUpdate;
+    private Date dueDateToUpdate;
+    private Boolean isAssignedToUpdate;
+
+
+
+
+
+
+
+
 }
