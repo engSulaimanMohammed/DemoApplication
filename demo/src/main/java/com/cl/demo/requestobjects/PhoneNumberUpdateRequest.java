bc44cb0c1a5717bed19bca16644ce79d904e8ee0
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-30 09:24:46 +0400

    feat(phone): add phone number update request

--- demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberUpdateRequest.java
+++ demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberUpdateRequest.java
@@ -1,4 +1,15 @@
 package com.cl.demo.requestobjects;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
+
 public class PhoneNumberUpdateRequest {
+    private UUID uuid; // Specify which phone number we want to edit.
+    private String countryCodeToUpdate;
+    private Long phoneNumberToUpdate;
 }
