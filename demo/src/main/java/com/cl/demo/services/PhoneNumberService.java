9f8abb0f134c60379861c1e38a963da80fcd5e4d
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-30 09:48:23 +0400

    feat(phone): add phone number service and create method

--- demo/src/main/java/com/cl/demo/services/PhoneNumberService.java
+++ demo/src/main/java/com/cl/demo/services/PhoneNumberService.java
@@ -1,4 +1,23 @@
 package com.cl.demo.services;
 
+import com.cl.demo.DemoApplication;
+import com.cl.demo.entities.PhoneNumber;
+import com.cl.demo.requestobjects.PhoneNumberCreateRequest;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
 public class PhoneNumberService {
+    public PhoneNumber addPhoneNumber(PhoneNumberCreateRequest requestObj) {
+        PhoneNumber phoneNumber = new PhoneNumber();
+
+        phoneNumber.setId(UUID.randomUUID());
+        phoneNumber.setIsActive(Boolean.TRUE);
+        phoneNumber.setCreatedDate(new Date());
+        phoneNumber.setCountryCode(requestObj.getCountryCode());
+        phoneNumber.setPhoneNumber(requestObj.getPhoneNumber());
+        DemoApplication.PhoneNumber_List.add(phoneNumber);
+        return phoneNumber;
+    }
 }
