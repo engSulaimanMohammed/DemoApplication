faa8011b9843298fda234f31a0ac59bd5b53ab29
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-30 09:43:32 +0400

    feat(phone): add phone number update response

--- demo/src/main/java/com/cl/demo/responseobjects/PhoneNumberUpdateResponse.java
+++ demo/src/main/java/com/cl/demo/responseobjects/PhoneNumberUpdateResponse.java
@@ -1,4 +1,23 @@
 package com.cl.demo.responseobjects;
 
+
+import com.cl.demo.entities.PhoneNumber;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class PhoneNumberUpdateResponse {
+    String phoneNumberId;
+    String countryCode;
+    Long phoneNumber;
+
+    public static PhoneNumberUpdateResponse convert(PhoneNumber phoneNumber) {
+        PhoneNumberUpdateResponse response = new PhoneNumberUpdateResponse();
+        response.setPhoneNumberId(phoneNumber.getId().toString());
+        response.setCountryCode(phoneNumber.getCountryCode());
+        response.setPhoneNumber(phoneNumber.getPhoneNumber());
+        return response;
+    }
 }
