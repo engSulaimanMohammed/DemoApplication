f31b800734a504d0d7b18731e7ac5f22d7e3ce80
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-30 09:36:27 +0400

    feat(phone): add phone number create response

--- demo/src/main/java/com/cl/demo/responseobjects/PhoneNumberCreateResponse.java
+++ demo/src/main/java/com/cl/demo/responseobjects/PhoneNumberCreateResponse.java
@@ -1,4 +1,31 @@
 package com.cl.demo.responseobjects;
 
+import com.cl.demo.entities.PhoneNumber;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class PhoneNumberCreateResponse {
+    String phoneNumberId;
+    String countryCode;
+    Long phoneNumber;
+
+    public static PhoneNumberCreateResponse convert(PhoneNumber phoneNumber) { // convert one phone number.
+        PhoneNumberCreateResponse response = new PhoneNumberCreateResponse();
+        response.setPhoneNumberId(phoneNumber.getId().toString());
+        response.setCountryCode(phoneNumber.getCountryCode());
+        response.setPhoneNumber(phoneNumber.getPhoneNumber());
+        return response;
+    }
+    public static List<PhoneNumberCreateResponse> convert(List<PhoneNumber> phoneNumberList) { // convert all phone number list.
+        List<PhoneNumberCreateResponse> responseList = new ArrayList<>();
+        for (PhoneNumber p : phoneNumberList) {
+            responseList.add(convert(p));
+        }
+        return responseList;
+    }
 }
